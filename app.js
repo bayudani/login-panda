2d781d197888002e6cd89b4e1616708fe220a336
bayudani <bayu22122017@gmail.com>
2023-12-31 12:13:57 +0700

    push

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+function fnView() {
+  const passwordField = document.getElementById("pass");
+  const eyeIcon = document.getElementById("btnView");
+
+  if (passwordField.value.length > 0 && passwordField.type === "password") {
+    passwordField.type = "text";
+    eyeIcon.innerHTML = '<i class="fa-regular fa-eye"></i>';
+    eyeIcon.title = 'sembunyikan password';
+  } else {
+    passwordField.type = "password";
+    eyeIcon.innerHTML = '<i class="fa-regular fa-eye-slash"></i>';
+    eyeIcon.title = 'lihat password';
+  }
+}
+
+
+
